6f1d1970f2a6dbee99eccb75fc9b8a3c4059ee7d
funkygao <funky.gao@gmail.com>
2014-11-24 14:37:23 +0800

    cleanup

--- const.go
+++ const.go
@@ -1,20 +1,14 @@
 package etclib
 
 const (
-	DIR_PROJECT = "proj"
-	DIR_NODES   = "node"
-
+	DIR_PROJECT  = "proj"
+	DIR_NODES    = "node"
 	DIR_MAINTAIN = "maintain"
 )
 
 const (
 	NODE_FAE   = "fae"
 	NODE_ACTOR = "act"
-)
-
-const (
-	OP_BOOT = iota + 1
-	OP_SHUTDOWN
 )
 
 const (
